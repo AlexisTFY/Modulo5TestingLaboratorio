338063171d287f2e002176f5c2f002e590a8bea3
Alexis Domínguez <adominguez@knowledgefy.com>
2022-10-15 16:45:41 +0200

    Test del componente Spinner

--- src/common/components/spinner/spinner.component.tsx
+++ src/common/components/spinner/spinner.component.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { usePromiseTracker } from 'react-promise-tracker';
-import Modal from '@material-ui/core/Modal';
+import { Modal } from '@mui/material';
 import Loader from 'react-spinners/ScaleLoader';
 import * as classes from './spinner.styles';
 
@@ -8,7 +8,10 @@ export const SpinnerComponent: React.FunctionComponent = () => {
   const { promiseInProgress } = usePromiseTracker();
   return (
     <Modal open={promiseInProgress} className={classes.modal}>
-      <div className={classes.loaderContainer}>
+      <div
+        className={classes.loaderContainer}
+        role="progressbar"
+      >
         <Loader />
       </div>
     </Modal>
